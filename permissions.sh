82fb53950f51934c0dc7cb802e8c991c43e229d0
LBZ <12958952+lbz-life@users.noreply.github.com>
2020-03-31 09:41:24 -0600

    Create permissions.sh

---
+++ permissions.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+# set root path
+path=$(echo $(pwd))
+
+# give apache ownership
+chown -R apache:apache ${path}
+echo "Chown set"
+
+# set correct file permissions
+find ${path} -type f -exec chmod 644 {} \;
+echo "File permissions set"
+
+# set correct directory permissions
+find ${path} -type d -exec chmod 755 {} \;
+echo "Directory permissions set"
+
+# set permissions to storage and bootstrap/cache
+chgrp -R apache ${path}/storage ${path}/bootstrap/cache
+chmod -R ug+rwx ${path}/storage ${path}/bootstrap/cache
+echo "Permissions set for storage and bootstrap/cahce directories"
+
+# set selinux permissions
+semanage fcontext -a -t httpd_sys_rw_content_t "${path}/storage(/.*)?"
+semanage fcontext -a -t httpd_sys_rw_content_t "${path}/bootstrap/cache(/.*)?"
+restorecon -Rv /var/www/
+echo "Selinux permissions set"
+
+echo "Permission process complete"
